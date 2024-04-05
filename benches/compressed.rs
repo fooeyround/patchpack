deb254b0e523ac1f06d0700cfcaa9abb772558bc
Jack <DevJackMC@gmail.com>
2024-04-05 11:36:15 -0700

    update dependencies

---
+++ benches/compressed.rs
@@ -0,0 +1,29 @@
+#![feature(test)]
+
+extern crate test;
+
+use std::io::{Read, Result};
+use test::Bencher;
+
+use liblzma::{decode_all, encode_all};
+
+
+
+
+
+
+
+fn compress_bench(x: &[u8], b: &mut Bencher) {
+    b.iter(|| {
+        encode_all(x, 9).unwrap()
+    });
+}
+
+fn decompress_bench(x: &[u8], b: &mut Bencher) {
+    b.iter(|| {
+        decode_all(x).unwrap()
+    });
+}
+
+
+
